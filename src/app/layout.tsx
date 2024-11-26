2d02c9bd87552882120517365f2bd886c8aafc7a
Sooraj Gupta <soorajgupta00@gmail.com>
2024-11-27 02:27:57 +0530

    auth is done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { TopNav } from "./_component/TopNav";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
@@ -10,24 +12,17 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>T3 Gallery</div>
-      <div>Sign In</div>
-    </nav>
-  );
-}
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable} hydrated`}>
-      <body className="vsc-initialized flex flex-col gap-4">
-        <TopNav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable} hydrated`}>
+        <body className="vsc-initialized flex flex-col gap-4">
+          <TopNav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
